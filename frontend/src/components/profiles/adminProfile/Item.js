ae62df939fb832556793f189914d4f870e7770b9
Nimit3-droid <Nimit3-droid@users.noreply.github.com>
2021-02-28 18:09:10 +0530

    backup

--- frontend/src/components/profiles/adminProfile/Item.js
+++ frontend/src/components/profiles/adminProfile/Item.js
@@ -1,40 +1,36 @@
-import React from 'react'
-import { Box ,Button} from '@material-ui/core'
+import React from 'react';
+import { Box, Button } from '@material-ui/core';
 
-
-
-function Item (props) {
-    var isClicked=true;
-        return (
-            <Box className='cards' style={{maxwidth: '18rem'}}>
-                <Box className="cardContent">
-                    <Box>
-                        <p>Item Name: {props.itemName}</p>
-                        <p>ID: {props.itemId}</p>
-                    </Box>
-                    <p>Requesting Date: {props.requestingDate}</p>
-                    <p>Due Date: {props.dueDate}</p>
+function Item(props) {
+    const isClicked = true;
+    return (
+        <Box className="cards" style={{ maxwidth: '18rem' }}>
+            <Box className="cardContent">
+                <Box>
+                    <p>Item Name: {this.props.itemName}</p>
+                    <p>ID: {this.props.itemId}</p>
                 </Box>
-                {isClicked===true?(<Box className='buttonAdmin'>
-                    <Button variant="contained" color="primary">Approve</Button>
-                    <Button variant="contained" color="primary">Deny</Button>    
-                </Box>):null}
-                
-                <Box className="cardContent">
-    
-                    <p>Request Name: {props.requestaName}</p>
-                    <p>Email ID: {props.emailID}</p>
-                    <p>Contact NO.: {props.contact}</p>
-                </Box>  
-                
+                <p>Requesting Date: {this.props.requestingDate}</p>
+                <p>Due Date: {this.props.dueDate}</p>
             </Box>
-    
-    
-    
-        );
-    
-  }
-  
+            {isClicked === true ? (
+                <Box className="buttonAdmin">
+                    <Button variant="contained" color="primary">
+                        Approve
+                    </Button>
+                    <Button variant="contained" color="primary">
+                        Deny
+                    </Button>
+                </Box>
+            ) : null}
 
+            <Box className="cardContent">
+                <p>Request Name: {this.props.requestaName}</p>
+                <p>Email ID: {this.props.emailID}</p>
+                <p>Contact NO.: {this.props.contact}</p>
+            </Box>
+        </Box>
+    );
+}
 
 export default Item;
